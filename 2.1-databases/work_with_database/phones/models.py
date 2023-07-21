7570a7c42cf8436cbd44bb4845cf5bda5cbfd09d
fomms <fomms@protonmail.com>
2023-07-21 19:28:34 +0300

    done hw

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,20 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField(max_length=40)
+    price = models.IntegerField()
+    image = models.URLField()
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField()
+
+    def __str__(self):
+        return f'{self.name}, {self.price}, {self.image}, {self.image}, {self.release_date}, {self.lte_exists}'
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
+
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from phones.models import Phone
 
 
 def index(request):
@@ -6,12 +7,17 @@ def index(request):
 
 
 def show_catalog(request):
+    sort_param = request.GET.get('sort', 'name')
     template = 'catalog.html'
-    context = {}
+    sort_dict = {'name': 'name', 'min_price': 'price', 'max_price': '-price'}
+    phones = Phone.objects.all().order_by(sort_dict[sort_param])
+    context = {'phones': phones}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone = Phone.objects.get(slug=slug)
+    context = {'phone': phone}
+    print(phone.name)
     return render(request, template, context)
