4ad10ce7b969dbd04c5880584f95444faee3a400
fomms <fomms@protonmail.com>
2023-08-15 16:14:57 +0300

    done HW

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,48 @@
 from django.contrib import admin
+from .models import Article, Tags, TagRelation
 
-from .models import Article
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+from pprint import pprint
 
 
-@admin.register(Article)
+class TagsInlinFormset(BaseInlineFormSet):
+    def clean(self):
+
+        main_tag_count = 0
+        tag_list = []
+        for form in self.forms:
+            pprint(form.cleaned_data)
+
+            if form.cleaned_data.get('main_tag'):
+                main_tag_count += 1
+
+            if main_tag_count > 1:
+                raise ValidationError('У статьи может быть только один главный тэг')
+
+            tag = form.cleaned_data.get('tags')
+            if tag in tag_list:
+                raise ValidationError('Уберите повторяющиеся тэги')
+
+            else:
+                tag_list.append(tag)
+
+        if main_tag_count == 0:
+            raise ValidationError('У статьи должен быть как минимум один главный тэг')
+
+        return super().clean()
+
+
+class TagsInline(admin.TabularInline):
+    model = TagRelation
+    extra = 1
+    formset = TagsInlinFormset
+
+
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    inlines = [TagsInline, ]
+    exclude = ('tags', )
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Tags)
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -1,12 +1,24 @@
 from django.db import models
 
 
+class Tags(models.Model):
+    name = models.CharField(max_length=64)
+
+    class Meta:
+        verbose_name = 'Тэг'
+        verbose_name_plural = 'Тэги'
+
+    def __str__(self):
+        return self.name
+
+
 class Article(models.Model):
 
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    tags = models.ManyToManyField(Tags, through='TagRelation', related_name='articles')
 
     class Meta:
         verbose_name = 'Статья'
@@ -14,3 +26,14 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class TagRelation(models.Model):
+    tags = models.ForeignKey(Tags, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    main_tag = models.BooleanField()
+
+    class Meta:
+        ordering = ['tags', ]
+        verbose_name = 'Тэг'
+        verbose_name_plural = 'Тэги'
